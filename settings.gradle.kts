110dae67a0cac8e7e53e5a211150489324596558
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-12-16 20:15:53 +1300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,44 @@
+import com.android.build.api.dsl.SettingsExtension
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version("3.16")
+    id("com.android.settings") version("8.2.0")
+}
+
+rootProject.name = "Compose Log Viewer"
+
+//include(
+//    ":core",
+//    ":material3",
+//)
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        if (System.getenv("CI") == "true") {
+            termsOfServiceAgree = "yes"
+            isUploadInBackground = false
+        }
+    }
+}
+
+configure<SettingsExtension> {
+    buildToolsVersion = "34.0.0"
+    compileSdk = 34
+    minSdk = 28
+}
